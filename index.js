44c9ba54cfe68f599f0ae281a94da7a60907e00e
bavarianrhino <rjriesenberger@gmail.com>
2019-10-02 16:14:11 -0400

    added beginning of google logic keys and access tokens

--- index.js
+++ index.js
@@ -7,12 +7,24 @@
 const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
 const app = express();
 
-passport.use(new GoogleStrategy());
+passport.use(
+    new GoogleStrategy(
+        {
+            clientID: keys.googleClientID,
+            clientSecret: keys.googleClientSecret,
+            callbackURL: '/auth/google/callback'
+            //third option/argument is the path url that the user is sent to after granting permission from google server
+        }, (accessToken) => {
+            console.log(accessToken);
+        }
+    )
+);
 
 
-const PORT = process.env.PORT || 3000/; // Heroku uses environment variables to tell us which port to use. Not needed if run on local machine, but handles if run on local machine
+const PORT = process.env.PORT || 3000/ // Heroku uses environment variables to tell us which port to use. Not needed if run on local machine, but handles if run on local machine
 app.listen(PORT)
 
