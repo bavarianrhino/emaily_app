a04e0eb24d51baea45fcea463cc007e4a8395702
bavarianrhino <rjriesenberger@gmail.com>
2019-10-05 21:56:20 -0400

    wired up cookie auth and cleaned up comments

--- index.js
+++ index.js
@@ -1,43 +1,27 @@
 
-// import express from 'express'; //ES6 This makes use of 2016 modules and as far as this project goes we are using common imports
 const express = require('express');
 const mongoose = require('mongoose')
+const cookieSession = require('cookie-session');
+const passport = require('passport') // Tells passport to keep of track of user authentication state by using cookie session - NOT FOR GOOGLE AUTH
 const keys = require('./config/keys')
-// const passportConfig = require('./services/passport'); // This is shortend to line below
-require('./models/User');
+require('./models/User'); // Shorthand for const passportConfig = require('./services/passport');
 require('./services/passport');
 
 mongoose.connect(keys.mongoURI)
 
 const app = express();
 
-// const authRoutes = require('./routes/authRoutes')
-// authRoutes(app);
-require('./routes/authRoutes')(app); // Replaces and short for above lines
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 1000, //30days 24hours 60mins/hr 1000ms/min
+        keys: [keys.cookieKey] // Encrypts time so cannot be manually changed, and array is always used if dev wants to use multiple keys from list that are randomly selected
+    })
+);
 
+app.use(passport.initialize());
+app.use(passport.session());
 
-
-// passport.use(
-//     new GoogleStrategy(
-//         {
-//             clientID: keys.googleClientID,
-//             clientSecret: keys.googleClientSecret,
-//             callbackURL: '/auth/google/callback' //third option/argument is the path url that the user is sent to after granting permission from google server
-//         }, (accessToken, refreshToken, profile, done) => {
-//             console.log('Access Token', accessToken);
-//             console.log('Refresh Token', refreshToken);
-//             console.log('Profile', profile);
-//         }
-//     )
-// );
-
-// app.get('/auth/google', passport.authenticate('google', {
-//         scope: ['profile', 'email']
-//     })
-// );
-
-// app.get('/auth/google/callback', passport.authenticate('google'));
-
+require('./routes/authRoutes')(app); // Short for const authRoutes = require('./routes/authRoutes'); authRoutes(app);
 
 const PORT = process.env.PORT || 3000; // Heroku uses environment variables to tell us which port to use. Not needed if run on local machine, but handles if run on local machine
 app.listen(PORT)
