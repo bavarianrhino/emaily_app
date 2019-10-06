1387c4bb83837f7a993db6c3528fa04ad37e9518
bavarianrhino <rjriesenberger@gmail.com>
2019-10-05 21:39:22 -0400

    added passport function to deserialize user

--- services/passport.js
+++ services/passport.js
@@ -6,9 +6,17 @@ const keys = require('../config/keys');
 const User = mongoose.model('users')
 
 // Creates identifying data for user record logging in
-passport.serializerUser((user, done) => { //This is the user instance created or retrieved from existing record from logic below
+passport.serializeUser((user, done) => { //This is the user instance created or retrieved from existing record from logic below
     done(null, user.id)  //user.id is automatically made and assigned when record is made in mongo...like what rails/activeRecord does
     // mongoDB shows record as _id:ObjectId("5d98212e856da17b9e522bcc") ... this can be referenced as simply user.id
+})
+
+passport.deserializeUser((id, done) => { //This is now an ID that we want to turn back into a user model instance
+    User.findById(id).then(user => {
+        done(null, user);
+    }).catch((e) => {
+		console.log('ERROR Returning User after deserializeUser: ', e.message);
+	});
 })
 
 passport.use(
