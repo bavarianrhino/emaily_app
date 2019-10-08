36659eb31cb6a9b9852de0f6d98643faa10c84c1
bavarianrhino <rjriesenberger@gmail.com>
2019-10-08 02:30:03 -0400

    added catch all proxy and axios action

---
+++ client/src/actions/index.js
@@ -0,0 +1,17 @@
+// ***************************************************************
+// After writing each one, write additional /api/* proxy in package.json
+// ***************************************************************
+
+import axios from 'axios';
+import { FETCH_USER } from './types';
+
+// REFACTOR LATER
+
+const fetchUser = () => {
+    return function(dispatch) {
+        axios
+            .get('/api/current_user')
+            .then(res => dispatch({ type: FETCH_USER, payload: res }))
+    };
+};
+// After writing each one, write additional proxy in package.json
