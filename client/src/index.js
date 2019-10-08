a21de2e0217e2e95a15507c4ee4e8aec64cebf90
bavarianrhino <rjriesenberger@gmail.com>
2019-10-07 23:00:26 -0400

    added devtools RootReducer instead of index

--- client/src/index.js
+++ client/src/index.js
@@ -2,20 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';  // react-redux - allows us to reach global store with provider Tag
 import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+// import thunk from 'redux-thunk';
 
 import App from './components/App';
+import { RootReducer } from './reducers/RootReducer';
 import * as serviceWorker from './serviceWorker';
 
 // import './index.css';
 // import 'semantic-ui-css/semantic.min.css';
 
-// import { RootReducer } from './Reducers/RootReducer';
-// import { composeWithDevTools } from 'redux-devtools-extension';
-// import thunk from 'redux-thunk';
 
 // const store = createStore(RootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 // const store = createStore(RootReducer, composeWithDevTools(applyMiddleware(thunk)));
-const store = createStore(() => [], {}, applyMiddleware())
+// const store = createStore(RootReducer, {}, applyMiddleware())
+const store = createStore(RootReducer, composeWithDevTools(applyMiddleware()));
 
 ReactDOM.render(
 	<Provider store={store}>
