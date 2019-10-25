eb3556092b82ea028ac5b104089969fb03956917
bavarianrhino <rjriesenberger@gmail.com>
2019-10-25 16:42:41 -0400

    catch all for routing and production ready

--- index.js
+++ index.js
@@ -26,6 +26,20 @@ app.use(passport.session());
 require('./routes/authRoutes')(app); // Short for const authRoutes = require('./routes/authRoutes'); authRoutes(app);
 require('./routes/billingRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+    // Need to make sure Express serves up production assets in Heroku
+    // Directly Below - If and get request comes in for some route or some file and we don't know what it is,
+    // it will direct it to the client/build directory to see if there's a file that matches the request at hand.
+    app.use(express.static('client/build'))
+
+    // Express will serve ip the index.html file if it doesn't recognize the route, we will assume the react-router side is
+    // responsible and we'll kick the user to the client side. **THIS IS OUR CATCH ALL FOR REQUESTS**
+    const path = require('path')
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
+
 const PORT = process.env.PORT || 3001; // Heroku uses environment variables to tell us which port to use. Not needed if run on local machine, but handles if run on local machine
 app.listen(PORT)
 
