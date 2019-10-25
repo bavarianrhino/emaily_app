be148e934ede011c13c4807e7a962f56748eb28b
bavarianrhino <rjriesenberger@gmail.com>
2019-10-25 04:15:24 -0400

    displayed users credits in nav

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -22,6 +22,7 @@ class Header extends Component {
                 // return <li><a href="/api/logout">Logout</a></li>
                 return [
                     <li key={1}><Payments /></li>,
+                    <li key={3} style={{ margin: '0 10px'}}>Credits: {this.props.auth.credits}</li>,
                     <li key={2}><a href="/api/logout">Logout</a></li>
                 ]
         }
