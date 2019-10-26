5a1c45d7f7c535a87123cb8dbce90e2437f01565
bavarianrhino <rjriesenberger@gmail.com>
2019-10-26 14:53:01 -0400

    uncommented logic in keysJS

--- config/keys.js
+++ config/keys.js
@@ -1,10 +1,10 @@
 // keys.js - figure out what credentials to return?
-// if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
     // we are in production - return production keys
-    // module.exports = require('./prod');
-// } else {
+    module.exports = require('./prod');
+} else {
     // we are in dev - return development keys!!
-    // module.exports = require('./dev');
-// }
-module.exports = require('./dev');
+    module.exports = require('./dev');
+}
+// module.exports = require('./dev');
 
