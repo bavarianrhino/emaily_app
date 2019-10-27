3f16d10de5659a32f6cc741ad4717f89e5f22ae1
bavarianrhino <rjriesenberger@gmail.com>
2019-10-27 03:04:25 -0400

    addClickTracking and addRecipients helper functions

--- services/Mailer.js
+++ services/Mailer.js
@@ -14,18 +14,43 @@ class Mailer extends helper.Mail {
         super();
 
         // We do everything below in such a way because sendgrid says to do so
-        this.from_email = new helper.Email('ryanriesenberger-no-reply@emaily.com');
+        //     - formatAddresses is a helper function below to iterate over the recipients subcollection
+        //     which is an array of objects....in which to extract each recipient email
+        this.from_email = new helper.Email('ryan-riesenberger-no-reply@emaily.com');
         this.subject = subject;
         this.body = new helper.Context('text/html', content);
-        this.recipients = this.formatAddresses(recipients);
-        // formatAddresses is a helper function below to iterate over the recipients subcollection
-        // which is an array of objects....in which to extract each recipient email
+        this.recipients = this.formatAddresses(recipients); // Array of helper email objects
+
+        this.addContent(this.body) // Mail base class has built in functions
+        this.addClickTracking();
+        this.addRecipients();
     }
 
     formatAddresses(recipients) {
         return recipients.map(({ email }) => {
             return new helper.Email(email);
         })
+    }
+
+    // Sendgrid Documentation for code below basically states this is what you need to do...so write this code
+    // Not much explanation on what's going on behind the scenes
+    addClickTracking() {
+        const trackingSettings = new helper.TrackingSettings();
+        const clickTracking = new helper.ClickTracking(true, true);
+
+        trackingSettings.setClickTracking(clickTracking);
+        this.addTrackingSettings(trackingSettings);
+    }
+
+    // Helper function that takes the array of helper email objects that were first run through,
+    // formatAddresses and does strange sendgrid things...
+    addRecipients() {
+        const personalize = new helper.Personalization();
+
+        this.recipients.forEach(recipient => {
+            personalize.addTo(recipient);
+        })
+        this.addPersonalization(personalize)
     }
 }
 module.exports = Mailer;
