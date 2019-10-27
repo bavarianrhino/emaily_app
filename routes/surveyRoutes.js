bc5fb717a8bf687acc4e42694099817ca88f8514
bavarianrhino <rjriesenberger@gmail.com>
2019-10-26 21:00:55 -0400

    added requireCredits middleware and used in surveyRoutes requester logic

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,1 +1,11 @@
-module.exports = app => {};
+const requireLogin = require('../middlewares/requireLogin');
+const requireCredits = require('../middlewares/requireCredits');
+
+module.exports = app => {
+    // 2 things to vet about user before hooking up.
+    // first user needs to be logged in and authenticated - use middleware 
+    // second that user has enough credits 
+    app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
+
+    })
+};
