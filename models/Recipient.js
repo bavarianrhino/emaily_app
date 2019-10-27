26cb7b9aee79ea1d523f843c1609e6f887f015f2
bavarianrhino <rjriesenberger@gmail.com>
2019-10-26 20:33:06 -0400

    added recipient model and connected Surveys to Users model

---
+++ models/Recipient.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+// const Schema = mongoose.Schema; is destructured below
+const { Schema } = mongoose;
+
+const recipientSchema = new Schema({
+    email: String,
+    responded: { type: Boolean, default: false }
+});
+
+module.exports = recipientSchema;
+
+// LOOK AT MONGOOSE DOCUMENTATION TO SEE MORE OPTIONS FOR SCHEMA
--- models/Survey.js
+++ models/Survey.js
@@ -1,12 +1,18 @@
 const mongoose = require('mongoose');
 // const Schema = mongoose.Schema; is destructured below
 const { Schema } = mongoose;
+const RecipientSchema = require('./Recipient')
 
 const surveySchema = new Schema({
     tile: String,
     body: String,
     subject: String,
-    recipients: [String]
+    recipients: [RecipientSchema],
+    yes: { type: Number, default: 0 },
+    no: { type: Number, default: 0 },
+    _user: { type: Schema.Types.ObjectId, ref: 'User' },
+    dateSent: Date,
+    lastResponded: Date
 });
 
 mongoose.model('surveys', surveySchema);
