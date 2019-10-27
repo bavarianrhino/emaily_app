3005d631f31728165b6c49ec5ec9783e7d1d7ed9
bavarianrhino <rjriesenberger@gmail.com>
2019-10-27 01:47:13 -0400

    sendgrid keys added dev and prod

--- config/prod.js
+++ config/prod.js
@@ -1,10 +1,12 @@
 // prod.js - production keys here!!
+// Making a reference to our key that is sitting over on heroku
 module.exports = {
 	googleClientID: process.env.GOOGLE_CLIENT_ID,
 	googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
 	mongoURI: process.env.MONGO_URI,
     cookieKey: process.env.COOKIE_KEY,
     stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
-    stripeSecretKey: process.env.STRIPE_SECRET_KEY
+    stripeSecretKey: process.env.STRIPE_SECRET_KEY,
+    sendGridKey: process.env.SEND_GRID_KEY
 };
 
