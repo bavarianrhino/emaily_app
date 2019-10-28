e03e2b45b163af03b7b3d1ac82abcb9b63531fc5
bavarianrhino <rjriesenberger@gmail.com>
2019-10-27 23:50:11 -0400

    started survey new component and connected it to app

---
+++ client/src/components/surveys/SurveyNew.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+// import { connect } from 'react-redux';
+
+// import { } from 'semantic-ui-react';
+
+class SurveyNew extends Component {
+    render() {
+        return (
+            <div>
+                Survey New!
+            </div>
+        )
+    }
+}
+
+
+// const mapStateToProps = (state) => {
+//     return {
+//         attr: state.attr
+//     }
+// }
+
+// const mapDispatchToProps = (dispatch) => ({
+//     functionName: (param) => dispatch({ type: 'ACTION_NAME', param })
+// })
+
+// export default connect(mapStateToProps, mapDispatchToProps)(name);
+export default SurveyNew
