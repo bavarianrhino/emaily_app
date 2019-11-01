6019bafcd7deb09f94f97349b22250a7ff563223
bavarianrhino <rjriesenberger@gmail.com>
2019-11-01 05:08:51 -0400

    cleaned up comments in actions :memo:

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,22 +1,18 @@
-// ***************************************************************
-// After writing each one, write additional /api/* proxy in package.json
-// ***************************************************************
 
 import axios from 'axios';
 import { FETCH_SURVEYS, FETCH_USER } from './types';
 
-// PREVIOUS REFACTORS BELOW
+// PREVIOUS REFACTORS IN NOTES
 export const fetchUser = () => async (dispatch) => {
     const res = await axios.get('/api/current_user');
 
-    // dispatch({ type: FETCH_USER, payload: res }); Passes entire object
     dispatch({ type: FETCH_USER, payload: res.data });
 };
 
 export const handleToken = (token) => async (dispatch) => {
     const res = await axios.post('/api/stripe', token);
     
-    dispatch({ type: FETCH_USER, payload: res.data }); // This uses FETCH_USER from above...I would break it up
+    dispatch({ type: FETCH_USER, payload: res.data });
 };
 
 export const submitSurvey = (values, history) => async dispatch => {
@@ -24,7 +20,7 @@ export const submitSurvey = (values, history) => async dispatch => {
 
     history.push('/surveys');
     console.log(history)
-    dispatch({ type: FETCH_USER, payload: res.data }); // This uses FETCH_USER from above...I would break it up
+    dispatch({ type: FETCH_USER, payload: res.data });
 };
 
 export const fetchSurveys = () => async (dispatch) => {
@@ -33,37 +29,6 @@ export const fetchSurveys = () => async (dispatch) => {
     dispatch({ type: FETCH_SURVEYS, payload: res.data });
 };
 
-// **Note if setupProxy.js is not in use, execute the following line.
-// After writing each one, write additional proxy in package.json
-
-// REFACTORED TO BELOW
-// export const fetchUser = () => {
-//     return function(dispatch) {
-//         axios
-//             .get('/api/current_user')
-//             .then(res => dispatch({ type: FETCH_USER, payload: res }))
-//     };
-// };
-
-// REFACTORED NUMBER 1
-// export const fetchUser = () => 
-//     function(dispatch) {
-//         axios
-//             .get('/api/current_user')
-//             .then(res => dispatch({ type: FETCH_USER, payload: res }))
-//     };
-
-// REFACTORED NUMBER 2
-// export const fetchUser = () => 
-//     (dispatch) => {
-//         axios
-//             .get('/api/current_user')
-//             .then(res => dispatch({ type: FETCH_USER, payload: res }))
-//     };
-
-// REFACTORED NUMBER 3
-// export const fetchUser = () => dispatch => {
-//     axios
-//         .get('/api/current_user')
-//         .then(res => dispatch({ type: FETCH_USER, payload: res }))
-// };
+// ************************************
+// Notes can be found in consolelog.js 
+// ************************************
