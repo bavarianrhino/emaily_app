080b58a8ba65616ba90057f770ff213b63b3d6fa
bavarianrhino <rjriesenberger@gmail.com>
2019-11-01 13:13:33 -0400

    moved add survey button into row and column

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -6,10 +6,14 @@ const Dashboard = () => {
     return (
         <div>
             <SurveyList />
-            <div className="fixed-action-btn">
-                <Link to="/surveys/new" className="btn-floating btn-large red">
-                    <i className="material-icons">add</i>
-                </Link>
+            <div className="row">
+                <div className="col s4 offset-s8">
+                    <div className="action-btn right">
+                        <Link to="/surveys/new" className="btn-floating btn-large waves-effect waves-light pulse red">
+                            <i className="material-icons">add</i>
+                        </Link>
+                    </div>
+                </div>
             </div>
         </div>
     );
