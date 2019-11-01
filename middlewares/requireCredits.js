8715ac50782cf9bece7639ef6410bf438a8b4782
bavarianrhino <rjriesenberger@gmail.com>
2019-11-01 04:34:13 -0400

    cleaned up comments :memo:

--- middlewares/requireCredits.js
+++ middlewares/requireCredits.js
@@ -1,10 +1,11 @@
-// Next is a function called once our middleware is complete and done running 
-// and then passes the request object to the next middleware
+
 module.exports = (req, res, next) => {
     if (req.user.credits < 1) {
-        return res.status(403).send({ error: "Please purchase more credits!"}) // Status code 403 Forbbiden can be changed to 402 Payment needed in future
+        return res.status(403).send({ error: "Please purchase more credits!"})
     }
 
-    // If user has more than 1 credit and next() is called, it passes along the request 
     next();
 };
+// ************************************
+// Notes can be found in consolelog.js 
+// ************************************
--- middlewares/requireLogin.js
+++ middlewares/requireLogin.js
@@ -1,10 +1,11 @@
-// Next is a function called once our middleware is complete and done running 
-// and then passes the reqest object to the next middleware
+
 module.exports = (req, res, next) => {
     if (!req.user) {
         return res.status(401).send({ error: "Please login in to purchase!"})
     }
 
-    // If logged in and next() is called, it passes along the request 
     next();
 };
+// ************************************
+// Notes can be found in consolelog.js 
+// ************************************
